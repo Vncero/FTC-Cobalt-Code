9e77cf117133b214c209462c3640e3d9a6a355e9
Vncero <92338199+Vncero@users.noreply.github.com>
2022-01-19 17:44:31 -0500

    Update AutonomousRedTop.java

--- AutonomousRedTop.java
+++ AutonomousRedTop.java
@@ -3,131 +3,86 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
+//import com.qualcomm.robotcore.util.
 
 @Autonomous(name="AutonomousRedTop")
-public class AutonomousRedTop extends AutonomousBase {
+public class AutonomousRedTop extends LinearOpMode {
     DcMotor FrontLeft;
     DcMotor BackLeft;
     DcMotor FrontRight;
     DcMotor BackRight;
     DcMotor CarouselMotor;
-    DcMotor LinearSlide;
+    CRServo Intake;
 
     private ElapsedTime runtime = new ElapsedTime();
 
+    final double DISTANCE_PER_SECOND = 104.25;
+    final double DEGREES_PER_SECOND = 350.0; // approximated
+
     final double ticksInARotation = 537.7;
-    final double theoreticalRadius = 10.9;
-    final double theoreticalMiddleExtension =  LinearSlideTicks(5.5);
-    final double theoreticalGroundExtension = LinearSlideTicks(3);
-    final double theoreticalFullExtension = (3 * ticksInARotation) - (LinearSlideTicks(5));
-
-    CRServo Intake;
-    Servo LSExtensionServo;
-
-    /* a lot of notes
-    the objective - get radius of turning circle
-    when the robot turns, the edges of the wheel hit points that make up its turning circle
-    relating the turning circle to the robot, we can deduce that the diameter of this circle can be found by
-    finding the diagonal measure between wheels, like FrontLeft and BackRight.
-    without being able to directly measure, we can estimate this diagonal by figuring out the robot's dimensions
-    we know that it fits between the barrier gap, meaning the width is at max 13.68in, use 13.65in
-    for length, the robot fits in the 2ft by 2ft squares, but the size limit is 18, so use 17in
-    now we have two measures for pythagorean theorem
-    13.65^2 + 17^2 = c^2
-    186.3225 + 289 = c^2
-    475.3225 = c^2
-    sqrt(475.3225) ~ 21.8in diameter
-    21.8 / 2 = 10.9in radius
-    * */
-
-    /* previous turn circle radius estimate
-    the robot must be within 18*18*18
-    therefore, the circle it rotates has diameter 18 at max
-    18 / 2 = 9
-    previously, I made the judgment that the circle it rotates needs the same area as the 18*18 square
-    however, this would create a circle larger than the square
-    anyway, here's the math
-    18*18 = 324 sq. in., max area of the circle
-    324 >= Math.PI * Math.pow(r, 2)
-    324 / Math.PI ~ 103.13240312354819
-    103.13240312354819 >~ Math.pow(r, 2)
-    Math.sqrt(103.13240312354819) ~ 10.155
-    10.155 >~ r
-    20.310 >~ d
-    round diameter down a little to 20, then circumference is about 62.83185
-    */
+    // possible: 18.0346888
+    final double theoreticalRadius = 10.2;
 
     @Override
-    public void runOpMode(){
+    public void runOpMode(){ 
+
         FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
         BackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
         FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
         BackRight = hardwareMap.get(DcMotor.class, "BackRight");
         Intake = hardwareMap.get(CRServo.class, "Intake");
-        LinearSlide = hardwareMap.get(DcMotor.class, "LinearSlide");
-        LSExtensionServo = hardwareMap.get(Servo.class, "LSExtensionServo");
+
         CarouselMotor = hardwareMap.get(DcMotor.class, "CarouselMotor");
 
         waitForStart();
         
-        super.setDriveTrain(FrontLeft, FrontRight, BackLeft, BackRight);
-        LSExtensionServo = super.setLSExtensionServo(LSExtensionServo);
-
-        LinearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        LinearSlide.setTargetPosition((int) theoreticalMiddleExtension);
+        // did not put this encoder reset in the directional methods, in case programmer wants flexiblity with keeping the encoder ticks at the same value. not sure why but there it instanceof
+        encoderMotorReset();
+        // Intake.setPower(-1);
         
-        LinearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        // sleep(2000);
         
-        LinearSlide.setPower(0.75);
+        // Intake.setPower(0);
         
-        while (LinearSlide.isBusy()) {}
+        // Forward(4, 0.2);
         
-        LinearSlide.setPower(0);
-
-        StrafeRight(0.5);
-        sleep(800);
-
-        Forward(0.5);
-        sleep(500);
+        // sleep(1000);
         
-        Stop();
+        // StrafeLeft(20, 0.2);
         
-        LinearSlide.setTargetPosition(0);
-        LinearSlide.setPower(0.7);
+        // Forward(50, 0.2);
         
-        while (LinearSlide.isBusy()) {}
+        // FrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+        // BackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+        // BackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+        // FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
         
-        Stop();
-    }
-
-    public enum Drive {
-        FORWARD,
-        TURN_LEFT,
-        TURN_RIGHT,
-        STRAFE_LEFT,
-        STRAFE_RIGHT
-    }
-
-
-    public int LinearSlideTicks(double inches) {
-        double diameter = 1.5;
-
-        double circumference = diameter * Math.PI; // might be wrong if it is then we're FUCKED !
-        // original measurement was 5in
-        //alt circumference ~ 4.75in.
-        double inchesPerTick = circumference / ticksInARotation;//approx 0.00929886553 inches per tick
-
-        return (int) Math.floor(inches / inchesPerTick);
+        // FrontLeft.setPower(0.5);
+        // FrontRight.setPower(-0.5);
+        // BackLeft.setPower(0.5);
+        // BackRight.setPower(-0.5);
+        
+        Forward(30, 0.1);
+        
+        telemetry.addLine("hi im here");
+        telemetry.update();
+        
+        // PROBLEM: carousel motor not attached/configured? figure out later
+        // CarouselMotor.setPower(1);
+        // sleep(1000);
+        // CarouselMotor.setPower(0);
+        // Forward(20, 0.25);
     }
 
     public double motorArcLength (int theta) {
         double rad = theta * (Math.PI / 180); //converts angle theta in degrees to radians
-        return rad * theoreticalRadius; //returns S, the arc length
-        /* old notes
+        return rad * theoreticalRadius; //isolates S, arc length
+        /*
         all the turning math is done on the assumption that driving a distance as a line
         is the same as driving that distance around a circumference
         as in, the turning motion does not counteract movement along the circumference
@@ -139,48 +94,113 @@ public class AutonomousRedTop extends AutonomousBase {
     }
 
     public int motorTicks (double inches) {
-        double diameter = 5.75;
+        double diameter = 3.5;
 
         double circumference = Math.PI * diameter;
 
-        double inchesPerTick = circumference / ticksInARotation; // approx 0.0204492733635192 inch
+        double inchesPerTick = circumference / ticksInARotation;
 
         return (int) Math.floor(inches / inchesPerTick);
     }
 
-    public void StrafeLeft (double Power) {
-        FrontLeft.setPower(-Power);
-        FrontRight.setPower(-Power);
-        BackLeft.setPower(Power);
-        BackRight.setPower(Power);
+    public double LinearSlideTicks(double inches) {
+
+        double circumference = 5.0; // might be wrong if it is then we're FUCKED !
+
+        double inchesPerTick = circumference / ticksInARotation;//approx 0.00929886553 ticks
+
+        return inches / inchesPerTick;
     }
 
-    public void StrafeRight (double Power) {
+    public void StrafeLeft (double inches, double Power) {
+    
+        int ticks = motorTicks(inches);
+        
+        FrontLeft.setTargetPosition(-ticks);
+        FrontRight.setTargetPosition(-ticks);
+        BackLeft.setTargetPosition(ticks);
+        BackRight.setTargetPosition(ticks);
+
+        runMotorEncoders();
+
+        FrontLeft.setPower(Power);
+        FrontRight.setPower(Power);
+        BackLeft.setPower(Power);
+        BackRight.setPower(Power);
+
+        waitForMotorEncoders();
+        
+    }
+
+    public void StrafeRight (double inches, double Power) {
+
+        encoderMotorReset();
+
+        setMotorTargets(motorTicks(inches));
+
+        runMotorEncoders();
+
         FrontLeft.setPower(Power);
         FrontRight.setPower(Power);
         BackLeft.setPower(-Power);
         BackRight.setPower(-Power);
+
+        waitForMotorEncoders();
     }
 
-    public void TurnLeft (double Power) {
+    public void TurnLeft (double inches, double Power) {
+        // both left sides go forward
+        // both right sides go backwards
+        // this makes the robot turn left and stationary
+
+        encoderMotorReset();
+
+        setMotorTargets(motorTicks(inches));
+
+        runMotorEncoders();;
+
         FrontLeft.setPower(-Power);
         BackLeft.setPower(-Power);
         FrontRight.setPower(-Power);
         BackRight.setPower(-Power);
+
+        waitForMotorEncoders();
     }
 
-    public void Forward (double Power) {
-        FrontLeft.setPower(Power);
-        FrontRight.setPower(-Power);
-        BackLeft.setPower(Power);
-        BackRight.setPower(-Power);
-    }
+    public void TurnRight (double inches, double Power) {
+        // both right sides go forward
+        // both left sides go backwards
 
-    public void TurnRight (double Power) {
+        encoderMotorReset();
+
+        setMotorTargets(motorTicks(inches));
+
+        runMotorEncoders();;
+
         FrontLeft.setPower(Power);
         BackLeft.setPower(Power);
         FrontRight.setPower(Power);
         BackRight.setPower(Power);
+
+        waitForMotorEncoders();
+    }
+
+    public void Forward (double inches, double Power) {
+        int ticks = motorTicks(inches);
+        
+        FrontLeft.setTargetPosition(ticks);
+        FrontRight.setTargetPosition(-ticks);
+        BackLeft.setTargetPosition(ticks);
+        BackRight.setTargetPosition(-ticks);
+
+        runMotorEncoders();
+
+        FrontLeft.setPower(Power);
+        FrontRight.setPower(Power);
+        BackLeft.setPower(Power);
+        BackRight.setPower(Power);
+
+        waitForMotorEncoders();
     }
 
     public void Stop () {
@@ -188,6 +208,8 @@ public class AutonomousRedTop extends AutonomousBase {
         FrontRight.setPower(0);
         BackLeft.setPower(0);
         BackRight.setPower(0);
+
+        encoderMotorReset();
     }
 
     public void encoderMotorReset() {
@@ -195,49 +217,18 @@ public class AutonomousRedTop extends AutonomousBase {
         FrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         BackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         BackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
+        
+        FrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-    public void setMotorTargets (int inches, Drive drive) {
-        encoderMotorReset();
-
-        int target = (int) motorTicks(inches);
-
-        switch (drive) {
-            case FORWARD:
-                FrontLeft.setTargetPosition(target);
-                FrontRight.setTargetPosition(-target);
-                BackLeft.setTargetPosition(target);
-                BackRight.setTargetPosition(-target);
-                break;
-            case TURN_LEFT:
-                FrontLeft.setTargetPosition(-target);
-                FrontRight.setTargetPosition(-target);
-                BackLeft.setTargetPosition(-target);
-                BackRight.setTargetPosition(-target);
-                break;
-            case TURN_RIGHT:
-                FrontLeft.setTargetPosition(target);
-                FrontRight.setTargetPosition(target);
-                BackLeft.setTargetPosition(target);
-                BackRight.setTargetPosition(target);
-                break;
-            case STRAFE_LEFT:
-                FrontLeft.setTargetPosition(-target);
-                FrontRight.setTargetPosition(-target);
-                BackLeft.setTargetPosition(target);
-                BackRight.setTargetPosition(target);
-                break;
-            case STRAFE_RIGHT:
-                FrontLeft.setTargetPosition(target);
-                FrontRight.setTargetPosition(target);
-                BackLeft.setTargetPosition(-target);
-                BackRight.setTargetPosition(-target);
-                break;
-        }
-
-        runMotorEncoders();
-
+    public void setMotorTargets (int motorTarget) {
+        FrontLeft.setTargetPosition(motorTarget);
+        FrontRight.setTargetPosition(motorTarget);
+        BackLeft.setTargetPosition(motorTarget);
+        BackRight.setTargetPosition(motorTarget);
     }
 
     public void runMotorEncoders () {
@@ -249,7 +240,12 @@ public class AutonomousRedTop extends AutonomousBase {
 
     public void waitForMotorEncoders () {
         while (FrontLeft.isBusy() && FrontRight.isBusy() && BackLeft.isBusy() && BackRight.isBusy()) {
-            idle();
+            telemetry.addLine("front left power is " + FrontLeft.getPower());
+            telemetry.addLine("front right power is " + FrontRight.getPower());
+            telemetry.addLine("back left power is " + BackLeft.getPower());
+            telemetry.addLine("back right power is " + BackRight.getPower());
+            
+            telemetry.update();
         }
 
         Stop();
