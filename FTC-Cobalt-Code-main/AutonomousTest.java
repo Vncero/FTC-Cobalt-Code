e50fc03af9377777e82dff8ffaa3c8534a9c470e
Vncero <92338199+Vncero@users.noreply.github.com>
2022-02-01 09:46:00 -0500

    Add files via upload

---
+++ FTC-Cobalt-Code-main/AutonomousTest.java
@@ -0,0 +1,155 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
+import java.util.List;
+
+@Autonomous(name = "test")
+public class AutonomousTest extends LinearOpMode {
+    DcMotor FrontLeft;
+    DcMotor BackLeft;
+    DcMotor FrontRight;
+    DcMotor BackRight;
+    DcMotor LinearSlide;
+    DcMotor CarouselMotor;
+    //alternatively,
+    // NormalizedColorSensor normColorSensor;
+    // ColorSensor colorSensor;
+    // DistanceSensor distanceSensor;
+
+    // NormalizedRGBA normRGBA;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    VuforiaLocalizer vuforia;
+    private static final String vuforiaLicenseKey = "AeHIiIj/////AAABmfU5ZY97Xk1Zu4akumUUm8JgsnypstBzT4OX7y1ZGLg4yuFGeCRDcHHXzsOkB2meJE3IdkM2mpq08BW54IWLTydXQ9PSFDSaXOQdFIcXePB+tLwRomI5lKbYAOxK16YjxwuKtJB43Ia5F1QwNZ+zjCO/VZWmcZlhyeVH0sKFxu1zHBgfw7xwnoNWww1U95lxhFspXtoAcvOH138ndfOZ5HlzXFnlLqaBG0esOXCJyowFvrJIvQ5pr3DW7NZBDNRRHfOr2TKWTRO9li+JIqI29640MI0Zrl/7RhcezrH3Gl+nIOYyKzbsmPDzcQVmokcAp5z4E6uZCiozbmhTBRdakDgKbXrXCJMWbMc8AUUTPshY";
+
+    TFObjectDetector tfObjectDetector;
+
+    private static final String TFOD_MODEL_ASSET = "FreightFrenzy_DM.tflite";
+    private static final String[] LABELS = {
+            "Duck",
+            "Marker"
+    };
+
+
+    @Override
+    public void runOpMode(){
+        // normColorSensor = hardwareMap.get(NormalizedColorSensor.class, "normColorSensor");
+        // colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
+        // distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
+
+        initVuforia();
+
+        initTensorFlow();
+
+        waitForStart();
+
+        // normRGBA = normColorSensor.getNormalizedColors();
+
+        // telemetry.addData("normRed", normRGBA.red);
+        // telemetry.addData("normGreen", normRGBA.green);
+        // telemetry.addData("normBlue", normRGBA.blue);
+        // telemetry.addData("normAlpha", normRGBA.alpha);
+
+        telemetry.update();
+
+        if (tfObjectDetector != null) {
+            tfObjectDetector.activate();
+        }
+
+        List<Recognition> updatedRecognitions = tfObjectDetector.getUpdatedRecognitions();
+
+        List<Recognition> duckRecognitions = null;
+
+        List<Recognition> nonDuckRecognitions = null;
+
+        if (updatedRecognitions != null) {
+            for (Recognition recognition : updatedRecognitions) {
+                if (recognition.getLabel().equals(LABELS[0])) {
+                    duckRecognitions.add(recognition);
+                    telemetry.addData("Confidence of recognition " +
+                            updatedRecognitions.indexOf(recognition) +  " (duck)", recognition.getConfidence());
+                } else {
+                    nonDuckRecognitions.add(recognition); //nonDuckRecognitions is likely only Marker recognitions: tfod model only has Duck and Marker
+                    telemetry.addData("Confidence of recognition " +
+                            updatedRecognitions.indexOf(recognition) + " (nonDuck)", recognition.getConfidence());
+                }
+
+            }
+
+            telemetry.addData("Ducks Detected", duckRecognitions.size());
+            telemetry.addData("Non-Ducks Detected", nonDuckRecognitions.size());
+            telemetry.update();
+
+            for (Recognition recognition : duckRecognitions) { //theoretically, there should only be one duck recognized but keep this in case
+                double frameWidth = recognition.getImageWidth(); //this should get the width of the frame from camera
+                double regionWidth = frameWidth / 3; //divide frame width into three sections of this width
+                //therefore, left is ~ 0 < recognition.getBottom() < regionWidth, middle ~ regionWidth < recognition.getBottom() < 2 * regionWidth
+                //and right is 2 * regionWidth < recognition.getBottom() < frameWidth (3 * regionWidth)
+                if (inRange(0, recognition.getBottom(), regionWidth)) { // use bottom to avoid bias in coordinate
+                    //duck is on left, leftmost barcode position means lowest level based on Freight Frenzy video, check how barcode is read
+                    telemetry.addData("Duck " + duckRecognitions.indexOf(recognition), "left");
+                }
+
+                if (inRange(2 * regionWidth, recognition.getBottom(), frameWidth)) {
+                    //duck is on right, rightmost barcode position means highest level based on Freight Frenzy video, check how barcode is read
+                    telemetry.addData("Duck " + duckRecognitions.indexOf(recognition), "right");
+                } else {
+                    //duck is in middle, middle barcode position means middle level based on Freight Frenzy video, check how barcode is read
+                    telemetry.addData("Duck " + duckRecognitions.indexOf(recognition), "middle");
+                }
+                telemetry.update();
+            }
+        }
+
+        if (tfObjectDetector != null) {
+            tfObjectDetector.deactivate();
+            tfObjectDetector.shutdown();
+        }
+
+        //put rest of autonomous below
+
+    }
+
+    public boolean inRange (double min, double value, double max) {
+        return min < value & value < max;
+    }
+
+    public void initVuforia() { //the two monitors may conflict, remove cameraMonitorViewId if necessary
+        int cameraMonitorViewId = hardwareMap.appContext.getResources()
+                .getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters vuParams = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        vuParams.vuforiaLicenseKey = vuforiaLicenseKey;
+        vuParams.cameraName = hardwareMap.get(WebcamName.class, "vuCam");
+
+        vuforia = ClassFactory.getInstance().createVuforia(vuParams);
+    }
+
+    public void initTensorFlow () {
+        int tfMonitorViewId = hardwareMap.appContext.getResources()
+                .getIdentifier("tfMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfParams = new TFObjectDetector.Parameters(tfMonitorViewId);
+        tfObjectDetector = ClassFactory.getInstance().createTFObjectDetector(tfParams, vuforia);
+
+        tfObjectDetector.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+    }
+}
+
---
+++ FTC-Cobalt-Code-main/TeleOP.java
@@ -0,0 +1,354 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+@TeleOp
+public class TeleOP extends OpMode {
+    DcMotor FrontLeft;
+    DcMotor BackLeft;
+    DcMotor FrontRight;
+    DcMotor BackRight;
+    DcMotor LinearSlide;
+    DcMotor CarouselMotor;
+    CRServo Intake;
+    Servo LSExtensionServo;
+    CRServo LSReleaseServo;
+
+    //approximately 3 rotations - "2cm"
+    //2cm ~ 0.787402 inches, 0.00929886553 / 0.787402 inches = ticks for 2cm (0.011809552856614936), 3*537.7 ~ 1613.1
+    // 1613.1 - 0.00732194531 ~ ticks for full extension if not we're f'd
+    // theoretically, we get an approximate number of ticks for the full thing
+
+    final double ticksInARotation = 537.7;
+
+    // final double theoreticalFullExtension = (3 * ticksInARotation) - (LinearSlideTicks(0.787402));
+    final double theoreticalFullExtension = (3 * ticksInARotation) - (LinearSlideTicks(5));
+
+    final double down = 0.25;
+    final double up = 0.93;
+
+    // official information says 3.1 rotations apparently
+    //https://www.gobilda.com/low-side-cascading-kit-two-stage-376mm-travel/
+    //top of the alliance shipping hub is 14.7, assuming the above is the correct slides, it reaches 14.8
+    //so alternate fullExtension to use is LinearSlideTicks(14.7);
+
+    final double theoreticalMiddleExtension =  LinearSlideTicks(5.5);
+    /*alliance shipping hub middle level top edge is 8.5 inches up,
+    assuming that the extension servo will cover the remaining height to dump freight in*/
+
+    final double theoreticalGroundExtension = LinearSlideTicks(3);
+    //if the ext doesn't already reach bottom level, use this
+
+    @Override
+    public void init() {
+        FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
+        BackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
+        FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        BackRight = hardwareMap.get(DcMotor.class, "BackRight");
+        Intake = hardwareMap.get(CRServo.class, "Intake");
+        CarouselMotor = hardwareMap.get(DcMotor.class, "CarouselMotor");
+        LinearSlide = hardwareMap.get(DcMotor.class, "LinearSlide");
+        // LinearSlideTopSensor = hardwareMap.get(TouchSensor.class, "LinearSlideTopTouchSensor");
+        LSExtensionServo = hardwareMap.get(Servo.class, "LSExtensionServo");
+
+        // LSExtensionServo.setPosition(0);
+        // IMPORTANT: 0.93 is top position, 0.25 is bottom position
+
+
+        telemetry.addData("Full LS Extension", theoreticalFullExtension);
+        telemetry.addData("Middle LS Extension", theoreticalMiddleExtension);
+        telemetry.addData("Ground LS Extension", theoreticalGroundExtension);
+
+        telemetry.addLine("Press right on the dpad if on red");
+        telemetry.addLine("Press start if on blue");
+
+        // need to clear it first - set to 0
+        encoderLSReset();
+
+        telemetry.addLine("DURING INIT - SET TARGET POSITION TO " + LinearSlide.getTargetPosition());
+
+        telemetry.update();
+    }
+
+    //if the init while doesn't work, use this init_loop()
+    @Override
+    public void init_loop () {
+        if (gamepad1.dpad_right) {
+            CarouselMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            telemetry.clear();
+
+            telemetry.addLine("Changed carousel direction to CW (red)");
+            telemetry.update();
+
+            //reverse motor, now turns CW - blue, dpad_right
+        } if (gamepad1.dpad_left) {
+            CarouselMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            telemetry.clear();
+
+            telemetry.addLine("Changed carousel direction to CCW (blue)");
+            telemetry.update();
+        }
+
+    }
+
+    @Override
+    public void loop () {
+
+        /* button config
+         *   gamepad1: movement on joysticks, carousel dir set (dpad) and activate (button)
+         *   gamepad2:  x intake, y top LS, b mid LS, a bot LS
+         * */
+
+        double c = gamepad1.left_stick_x;
+        double x = gamepad1.right_stick_x;
+        double y = -gamepad1.left_stick_y;
+
+        if (gamepad1.left_bumper) {
+            y *= 0.3;
+            c *= 0.3;
+            x *= 0.3;
+        } else {
+            y *= 0.9;
+            c *= 0.9;
+            x *= 0.9;
+        }
+
+        FrontLeft.setPower(y+x+c);
+        FrontRight.setPower(-y+x+c);
+        BackLeft.setPower(y+x-c);
+        BackRight.setPower(-y+x-c);
+
+        if (gamepad1.dpad_right) {
+            CarouselMotor.setPower(1);
+        }
+
+        else if (gamepad1.dpad_left) {
+            CarouselMotor.setPower(-1);
+        }
+
+        else CarouselMotor.setPower(0);
+
+        if (gamepad2.dpad_up) {
+            Intake.setPower(1);
+            telemetry.update();
+        }
+
+        else if (gamepad2.dpad_down) {
+            Intake.setPower(-1);
+        }
+
+        else Intake.setPower(0);
+
+        if (gamepad2.left_bumper) {
+            LSExtensionServo.setPosition(0.15);
+            //0
+            //moves to bot
+            // LSExtensionServo.setPosition(up);
+        }
+
+        else if (gamepad2.right_bumper) {
+            LSExtensionServo.setPosition(0.45);
+            //90
+            //zero
+            // LSExtensionServo.setPosition(down);
+        }
+
+        else if (gamepad2.right_trigger == 1) {
+            //180
+            LSExtensionServo.setPosition(0.8);
+        }
+
+        if (gamepad2.y) {
+            lsLevelSet(3);
+            LSExtensionServo.setPosition(0.8);
+        }
+
+        if (gamepad2.b) {
+            lsLevelSet(2);
+            LSExtensionServo.setPosition(0.45);
+
+            // telemetry.addData("Middle LS Extension", theoreticalMiddleExtension);
+        }
+
+        if (gamepad2.a) {
+
+            lsLevelSet(1);
+            telemetry.addData("Ground LS Extension", 0);
+        }
+
+        if (gamepad2.x) {
+            //level 1 dump freight
+
+            lsLevelSet(1);
+
+            LSExtensionServo.setPosition(0.45);
+        }
+
+        //make a default percentage to read at the whole game
+
+        telemetry.update();
+    }
+
+    // @Override
+    // public void stop() {
+    //     if (LSExtensionServo.getPosition() != 0.8) {
+    //         telemetry.addLine("changing extension position");
+    //         telemetry.update();
+    //         lsLevelSet(2);
+    //         LSExtensionServo.setPosition(0.8); //bot
+    //     }
+
+    //     lsLevelSet(1);
+
+    //     encoderLSReset();
+    // }
+
+    public void intakeFreight () { //intake method, theoretically completely loopable
+        if (LSExtensionServo.getPosition() != 0) { //if ext not at nec, change it to nec
+            lsLevelSet(2);
+        }
+
+        lsLevelSet(1);
+        LSReleaseServo.setPower(1);
+        //will intake freight, should allow driving to continue
+
+        //rel begins 0.5 after releasing: b pressed
+//        //LSReleaseServo.setPosition(0.5);
+//        lsLevelSet(1);
+//        LSExtensionServo.setPosition(0);
+//        Intake.setPower(1);
+//        lsLevelSet(3);
+//        Intake.setPower(0);
+//        LSExtensionServo.setPosition(0.5);
+//        LSReleaseServo.setPosition(0); //if necessary, tweak this to 0.25 in case bucket drops freight
+//        LSExtensionServo.setPosition(1); //if rel is tweaked to 0.25, find a point of ext to return rel to 0
+//        //rel can now go to 0.5 after pressing b
+    }
+
+    public void lsLevelSet (int level) { //method to move ls to preset levels
+
+        if (level == 1) { //ground
+
+            LinearSlide.setTargetPosition(0);
+            //Set the Linear Slide's target to the lowest, 0
+
+            LinearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //set the motor to run toward the position
+
+            LinearSlide.setPower(0.75); //set power to the motor
+
+            waitForLinearSlide(); //while loop for the encoders to run through
+
+            LinearSlide.setPower(0); //cut the encoder power
+        }
+
+        if (level == 2) { //middle
+            telemetry.addLine("target position before: " + LinearSlide.getTargetPosition());
+            LinearSlide.setTargetPosition((int) theoreticalMiddleExtension);
+
+            // telemetry.addLine("middle extension is " + theoreticalMiddleExtension);
+
+            LinearSlide.setPower(0.75);
+
+            telemetry.addLine("the target position should be " + theoreticalMiddleExtension + " , and is: " + LinearSlide.getTargetPosition());
+
+            waitForLinearSlide();
+
+            LinearSlide.setPower(0);
+        }
+
+        if (level == 3) { //top
+
+            LinearSlide.setTargetPosition((int) theoreticalFullExtension);
+
+            LinearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            LinearSlide.setPower(0.75);
+
+            waitForLinearSlide();
+
+            LinearSlide.setPower(0);
+        }
+    }
+
+    public void waitForLinearSlide() {
+        while (LinearSlide.isBusy()) {
+            telemetry.addData("Linear Slide at position", LinearSlide.getCurrentPosition());
+            telemetry.update();
+
+            double c = gamepad1.left_stick_x;
+            double x = gamepad1.right_stick_x;
+            double y = -gamepad1.left_stick_y;
+
+            if (gamepad1.left_bumper) {
+                y *= 0.3;
+                c *= 0.3;
+                x *= 0.3;
+            } else {
+                y *= 0.9;
+                c *= 0.9;
+                x *= 0.9;
+            }
+
+            FrontLeft.setPower(y+x+c);
+            FrontRight.setPower(-y+x+c);
+            BackLeft.setPower(y+x-c);
+            BackRight.setPower(-y+x-c);
+        }
+    }
+
+    public void encoderLSReset() {
+        LinearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void runLSEncoder() {
+        LinearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void encoderMotorReset() {
+        FrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void setMotorTargets (int motorTarget) {
+        FrontLeft.setTargetPosition(motorTarget);
+        FrontRight.setTargetPosition(motorTarget);
+        BackLeft.setTargetPosition(motorTarget);
+        BackRight.setTargetPosition(motorTarget);
+    }
+
+    public void runMotorEncoders () {
+        FrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public int LinearSlideTicks(double inches) {
+        double diameter = 1.5;
+
+        double circumference = diameter * Math.PI; // might be wrong if it is then we're FUCKED !
+        // original measurement was 5in
+        //alt circumference ~ 4.75in.
+        double inchesPerTick = circumference / ticksInARotation;//approx 0.00929886553 inches per tick
+
+        return (int) Math.floor(inches / inchesPerTick);
+    }
+
+    public double MotorTicks (double inches) {
+        double diameter = 3.75;
+
+        double circumference = Math.PI * diameter; // in inches
+
+        double inchesPerTick = circumference / 537.7; // approx 0.0204492733635192 inches per tick
+
+        return inches / inchesPerTick;
+    }
+}
