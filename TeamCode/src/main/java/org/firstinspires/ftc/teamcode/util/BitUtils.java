7b9c7be6d1c92ca6773d90d7922a8b816554a633
Vincent <vincentzheng1423@gmail.com>
2022-05-04 17:54:03 -0400

    pulled code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/BitUtils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/BitUtils.java
@@ -1,7 +1,7 @@
-package org.firstinspires.ftc.teamcode.opmodes.autonomous.base;
+package org.firstinspires.ftc.teamcode.util;
 
 public class BitUtils {
     public static boolean isBitIn(int whole, int check) {
-        return (whole & check) == check; 
+        return (whole & check) == check;
     }
 }
